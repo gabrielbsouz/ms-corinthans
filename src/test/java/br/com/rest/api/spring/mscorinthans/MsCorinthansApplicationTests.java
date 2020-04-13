92f702905010c22381019a1a21a4a2448acf1795
Gabriel Souza <gabriel.souza@aisdigital.com.br>
2020-04-13 17:24:49 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/rest/api/spring/mscorinthans/MsCorinthansApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.api.spring.mscorinthans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCorinthansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
